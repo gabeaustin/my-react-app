9e2f1294e019d59b601f777b4e31efe09272115c
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-24 10:07:50 -0500

    made query to mysql thru index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,39 @@
 const express = require("express");
+const db = require("./config/db");
 
 const app = express();
 
 const PORT = 3001;
 
+// put this here to test and see if is working
+// if so, use the db.query code to make the CREATE
+/* app.get("/", (req, res) => {
+    // res.send("server running");
+    // mySQL stmt to check to see if the database connection is working (below)
+    db.query(
+        "INSERT INTO posts (title, postText, userName) VALUES ('dictum', 'Ut dictum enim.', 'meme')"
+    );
+}); */
+
 app.get("/", (req, res) => {
-    res.send("server running");
+
 });
+
+app.post("/api/create", (req, res) => {
+    db.query(
+    "INSERT INTO posts (title, postText, userName) VALUES ()"), (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+    };
+});
+
+
+db.query(
+    "INSERT INTO posts (title, postText, userName) VALUES ('dictum', 'Ut dictum enim.', 'meme')"
+);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`);
