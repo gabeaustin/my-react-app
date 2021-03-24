30f4af66ce40017cf4ff94417bf71cef62e1634e
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-24 11:08:26 -0500

    added bootstrap

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import "../App.css"
 
 export default function CreatePost() {
     return (
         <div className="CreatePost">
             <div className="uploadPost">
                 <input type="text" />
-                <input type="textarea"/>
+                <textarea />
                 <input type="text"/>
             </div>
         </div>
