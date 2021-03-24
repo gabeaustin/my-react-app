cda80133eed55aa9769048fbbb9ab4728e52e594
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-24 00:18:16 -0500

    added css to style pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const App = () => {
     return (
-        <div>
-            <h1>Hey You!</h1>
-        </div>
+        <>
+            <div className="navbar">
+                <div className="links">
+                    <a href="/mainpage">Main Page</a>
+                    <a href="/createpost">Create Post</a>
+                </div>
+            </div>
+        </>
     );
 };
 
